282d6bd82509a2e483b9648efc0843afb955c765
Avi Aryan <avi.aryan123@gmail.com>
2017-08-03 16:16:02 +0530

    push

---
+++ push.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Use GitHub pages as a lite download hosting server
+
+# the max number of commits to keep after which version information is destroyed
+LIMIT=1
+
+# run transforms
+source ./transformations.sh
+
+# get commit count
+count=$(git rev-list --count HEAD)
+echo $count
+
+# new branch to overwrite when commit count more than limit
+if [ $count -gt $LIMIT ]; then
+	git checkout --orphan tempBranch
+fi
+
+git add .
+git commit -m 'push'
+
+# overwrite branch
+if [ $count -gt $LIMIT ]; then
+	git branch -D master
+	git branch -m master
+fi
+
+git push -f origin master
+git gc --aggressive --prune=all
---
+++ transformations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This file contains file transformations to be done
+# Runs before data is pushed
+
+# use -f to overwrite
+mv -f aviaryan_resume.pdf resume.pdf
